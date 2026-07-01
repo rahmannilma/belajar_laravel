e2f79e4ea5df45d2103b501a78d7e4feff0e2c4e
IbnMuhammad <rahmannilma@gmail.com>
2026-07-02 00:29:22 +0800

    print

--- resources/views/kasir/index.blade.php
+++ resources/views/kasir/index.blade.php
@@ -268,7 +268,7 @@
             <div class="px-4 pb-4 flex items-center justify-between">
                 <span class="text-sm font-medium text-gray-700 dark:text-gray-300">Cetak Struk Otomatis</span>
                 <label class="relative inline-flex items-center cursor-pointer">
-                    <input type="checkbox" x-model="autoPrint" @change="localStorage.setItem('auto_print', autoPrint)" class="sr-only peer">
+                    <input type="checkbox" x-model="autoPrint" class="sr-only peer">
                     <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-teal-500"></div>
                 </label>
             </div>
@@ -436,12 +436,17 @@ function posSystem() {
         isProcessing: false,
         countdown: 30,
         countdownInterval: null,
-        autoPrint: localStorage.getItem('auto_print') === 'true',
+        autoPrint: localStorage.getItem('auto_print') !== 'false',
 
         init() {
             this.filteredProducts = this.products;
             this.updateClock();
             setInterval(() => this.updateClock(), 1000);
+
+            // Simpan status cetak otomatis ke localStorage saat berubah
+            this.$watch('autoPrint', value => {
+                localStorage.setItem('auto_print', value);
+            });
         },
 
         updateClock() {
